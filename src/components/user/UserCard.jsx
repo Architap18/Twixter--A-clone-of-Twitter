4281dff34f5d60fbbcc38dee08e8d6708617ab4a
Aastha Musale <gsraastha5@gmail.com>
2026-04-20 11:46:00 +0530

    feat: add reusable user card component

---
+++ src/components/user/UserCard.jsx
@@ -0,0 +1,23 @@
+import Avatar from "../common/Avatar";
+
+const UserCard = ({ user, onFollow }) => {
+  if (!user) return null;
+
+  return (
+    <div className="user-card">
+      <div className="user-card__left">
+        <Avatar src={user.avatar} name={user.name} size={44} />
+        <div className="user-card__info">
+          <h4 className="user-card__name">{user.name}</h4>
+          <p className="user-card__username">@{user.username}</p>
+        </div>
+      </div>
+
+      <button className="user-card__follow" onClick={onFollow} type="button">
+        Follow
+      </button>
+    </div>
+  );
+};
+
+export default UserCard;
