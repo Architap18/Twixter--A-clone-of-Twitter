f93dc1680f8492e2b45a8c9cab7f2c2e62f82575
Architap18 <architapal1806@gmail.com>
2026-04-20 09:54:20 +0530

    Save work before pulling

--- src/components/layout/RightSidebar.jsx
+++ src/components/layout/RightSidebar.jsx
@@ -1,19 +1,42 @@
+import { useState, useEffect } from "react";
 const RightSidebar = () => {
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=b456d0a71b284942a6a1b24a24f11f0c")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.articles) {
+          setNews(data.articles.slice(0, 5));
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("News fetch failed:", err);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div className="rightSidebar">
       {/* News */}
       <div className="card">
-        <h3>Today's News</h3>
-        <div className="newsItem">
-          <p>David Miller's thrilling IPL win</p>
-          <span>2 days ago · 63K posts</span>
-        </div>
-        <div className="newsItem">
-          <p>Lenskart boycott controversy</p>
-          <span>2 days ago · 206K posts</span>
-        </div>
+      <h3>Today's News</h3>
+      {news.length > 0 ? (
+  news.map((article, index) => {
+    const posts = Math.floor(Math.random() * 90 + 10); // 10–100K
+    return (
+      <div className="newsItem" key={index}>
+        <p>{article.title}</p>
+        <span>
+          {posts}K posts •{" "}
+          {new Date(article.publishedAt).toLocaleDateString()}
+        </span>
       </div>
-      {/* Who to follow */}
+    );
+  })) : (
+      <p style={{ fontSize: "13px", color: "gray" }}> No news available</p>)}
+      </div>
+      {/* recommedations*/}
       <div className="card">
         <h3>Who to follow</h3>
         <div className="user">
@@ -33,7 +56,7 @@ const RightSidebar = () => {
         <div className="user">
           <div className="info">
             <strong>Narendra Modi</strong>
-            <span>@Narendra Modi</span>
+            <span>@narendramodi</span>
           </div>
           <button>Follow</button>
         </div>
--- src/pages/HomeFeed.jsx
+++ src/pages/HomeFeed.jsx
@@ -3,34 +3,19 @@ import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import TweetCompose from "../components/tweet/TweetCompose";
 import TweetCard from "../components/tweet/TweetCard";
-
 const HomeFeed = () => {
   const [activeTab, setActiveTab] = useState("forYou");
   const { tweets } = useContext(AppContext);
-
   return (
     <div className="home">
-
       {/* Tabs */}
       <div className="home__tabs">
-        <span
-          className={activeTab === "forYou" ? "active" : ""}
-          onClick={() => setActiveTab("forYou")}
-        >
-          For you
-        </span>
-
-        <span
-          className={activeTab === "following" ? "active" : ""}
-          onClick={() => setActiveTab("following")}
-        >
-          Following
-        </span>
+        <span className={activeTab === "forYou" ? "active" : ""}onClick={() => setActiveTab("forYou")}>For you</span>
+        <span className={activeTab === "following" ? "active" : ""}onClick={() => setActiveTab("following")} >
+          Following</span>
       </div>
-
       {/* Tweet Box */}
       <TweetCompose />
-
       {/* Feed */}
       <div className="feed">
         {tweets.map((tweet) => (
