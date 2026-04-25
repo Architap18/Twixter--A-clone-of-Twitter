362df6e8b796f03ceeadec0f8e6cbd8a4c65007b
Architap18 <architapal1806@gmail.com>
2026-04-25 21:29:37 +0530

    fix api and remove vercel config

--- api/news.js
+++ api/news.js
@@ -3,22 +3,16 @@ let lastFetchTime = {};
 
 export default async function handler(req, res) {
   const { category = "general" } = req.query;
-  const now = Date.now();
-  if (
-    cachedData[category] &&
-    now - (lastFetchTime[category] || 0) < 10 * 60 * 1000
-  ) {
-    return res.status(200).json(cachedData[category]);
-  }
+
   try {
     const url = `https://gnews.io/api/v4/top-headlines?country=in&lang=en&topic=${category}&apikey=${process.env.GNEWS_KEY}`;
 
     const response = await fetch(url);
     const data = await response.json();
-
-    // store cache per category
-    cachedData[category] = data;
-    lastFetchTime[category] = now;
+    res.setHeader(
+      "Cache-Control",
+      "s-maxage=600, stale-while-revalidate=300"
+    );
 
     res.status(200).json(data);
   } catch (error) {
